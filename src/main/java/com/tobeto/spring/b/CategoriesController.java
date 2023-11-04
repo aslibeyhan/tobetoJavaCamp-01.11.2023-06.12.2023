e7ba00b5806bf1ff2fbd2568da43f54541b1ed61
aslibeyhan <asli.beyhann@gmail.com>
2023-11-04 15:56:33 +0300

    Homework- 03.11.2023 olarak güncellendi

--- src/main/java/com/tobeto/spring/b/CategoriesController.java
+++ src/main/java/com/tobeto/spring/b/CategoriesController.java
@@ -2,26 +2,56 @@ package com.tobeto.spring.b;
 
 import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @RestController
 @RequestMapping("api/categories")
 public class CategoriesController {
+    List<Category> inMemoryList=new ArrayList<>();
+
     @GetMapping
-    public String get(){
-        return "Kategori Alındı";
+    public List<Category> get(){
+
+        return inMemoryList;
+    }
+
+    @GetMapping("{id}")
+    public Category getById(@PathVariable int id){
+    Category category=inMemoryList.stream()
+            .filter((p)->p.getId()==id)
+            .findFirst()
+            .orElseThrow();
+    return category;
+
     }
 
     @PostMapping
-    public String post(){
-        return "Kategori Eklendi";
+    public Category add(@RequestBody Category category){
+         inMemoryList.add(category);
+         return category;
     }
 
     @PutMapping
-    public  String put(){
-        return "Kategori Güncellendi";
+   public boolean updateCategory(@RequestBody Category category){
+        for(int i=0;i<inMemoryList.size();i++){
+            Category category1=inMemoryList.get(i);
+            if(category1.getId()==category.getId()){
+                inMemoryList.set(i,category);
+                return true;
+            }
+        }
+        return false;
     }
 
-    @DeleteMapping
-    public String delete(){
-        return "Kategori Silindi";
+    @DeleteMapping("/delete/{id}")
+    public boolean delete(@PathVariable int id){
+       boolean delete=inMemoryList.remove(inMemoryList.stream()
+               .filter((p)->p.getId()==id)
+               .findFirst()
+               .orElseThrow());
+        System.out.println(inMemoryList);
+        System.out.println("Person Deleted");
+        return delete;
     }
 }
--- src/main/java/com/tobeto/spring/b/ProductsController.java
+++ src/main/java/com/tobeto/spring/b/ProductsController.java
@@ -2,26 +2,53 @@ package com.tobeto.spring.b;
 
 import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @RestController
 @RequestMapping("api/products")
 public class ProductsController {
+    List<Product> inMemoryList=new ArrayList<>();
     @GetMapping
-    public String get(){
-        return "Ürün Alındı";
+    public List<Product> get(){
+        return inMemoryList;
+    }
+
+    @GetMapping("{id}")
+    public Product getById(@PathVariable int id){
+        Product product=inMemoryList.stream()
+                .filter((p)->p.getId()==id)
+                .findFirst()
+                .orElseThrow();
+        return product;
     }
 
     @PostMapping
-    public String post(){
-        return "Ürün Eklendi";
+    public Product add(@RequestBody Product product){
+        inMemoryList.add(product);
+        return product;
     }
 
     @PutMapping
-    public  String put(){
-        return "Ürün Güncellendi";
+    public  boolean updateProduct(@RequestBody Product product){
+        for(int i=0;i<inMemoryList.size();i++){
+            Product product1=inMemoryList.get(i);
+            if(product1.getId()==product.getId()){
+                inMemoryList.set(i,product);
+                return true;
+            }
+
+        }
+        return false;
     }
 
-    @DeleteMapping
-    public String delete(){
-        return "Ürün Silindi";
+    @DeleteMapping("{id}")
+    public boolean delete(@PathVariable int id){
+       boolean delete=inMemoryList.remove(inMemoryList.stream()
+               .filter((p)->p.getId()==id)
+               .findFirst()
+               .orElseThrow());
+        System.out.println(inMemoryList);
+        return delete;
     }
 }
