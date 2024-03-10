f5b9d5928b2b454c360897b6ffd26375504e1bcf
Aslı Beyhan <77741685+aslibeyhan@users.noreply.github.com>
2024-03-10 16:56:29 +0300

    all projects added

--- src/main/java/com/tobeto/spring/b/core/utilities/exceptions/GlobalExceptionHandler.java
+++ src/main/java/com/tobeto/spring/b/core/utilities/exceptions/GlobalExceptionHandler.java
@@ -1,17 +1,46 @@
 package com.tobeto.spring.b.core.utilities.exceptions;
 
 import org.springframework.http.HttpStatus;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.lang.reflect.Field;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
     //exceptionun typenı vermek ---> .class demesinin sebebi
     @ExceptionHandler({MethodArgumentNotValidException.class})
     @ResponseStatus(HttpStatus.BAD_REQUEST)
-    public String handleValidationException(){
-        return "Validasyon hatası";
+    //hata ne ise ona göre dönüş yapar bu yapı ile.
+    public Map<String,String> handleValidationException(MethodArgumentNotValidException exception)
+    {
+        Map<String ,String> errors=new HashMap<>();
+        for(FieldError error:exception.getBindingResult().getFieldErrors()){
+            errors.put(error.getField(), error.getDefaultMessage());
+        }
+        return errors;
+    }
+
+    @ExceptionHandler({RuntimeException.class})
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public String handeRuntimeError(RuntimeException exception ){
+        return exception.getMessage();
+    }
+
+
+
+    //Known Types
+    //Unknown Types
+    @ExceptionHandler({Exception.class})
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    public String handleUnknownErrors(){
+        return "Bilinmedik hata";
     }
 }
